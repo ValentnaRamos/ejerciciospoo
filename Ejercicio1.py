8162dd1464e61c6bd28ed0b68ba8a01c8cab349f
essay1 <saritaprinmc@gmail.com>
2024-02-15 17:22:57 -0500

    First exercise, first commit

---
+++ Ejercicio1.py
@@ -0,0 +1,6 @@
+#Ejercicio 1: Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla
+nombre = (input("Por favor ingrese su nombre"))
+edad = (input("Cuantos años tiene?"))
+
+print(f"Su nombre es",nombre)
+print(f"Su edad es",edad)
