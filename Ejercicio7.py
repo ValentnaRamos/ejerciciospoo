d3ebc1b651c7fa68ad9eb9d0be69def36b211a37
essay1 <saritaprinmc@gmail.com>
2024-02-15 17:38:21 -0500

    exercise #7,commit #7

---
+++ Ejercicio7.py
@@ -0,0 +1,4 @@
+#Ejercicio 7: Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada.
+
+num = [20,40,500,35,37]
+print("El número máximo de la lista es",max(num))
