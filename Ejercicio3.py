ed86b4019f7acce28ca2558f278506c5e390f598
essay1 <saritaprinmc@gmail.com>
2024-02-15 17:34:07 -0500

    exercise #3,commit #3

---
+++ Ejercicio3.py
@@ -0,0 +1,7 @@
+#Ejercicio 3:
+# Crear un programa que genere una lista de números aleatorios y los imprima en pantalla.
+import numpy as np
+import random
+
+num = np.random.random(6)
+print("El numero aleatorio es",num)
